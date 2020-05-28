4118d40ae539cce56492138befee254d270fb66a
zhangtj <1771988873@qq.com>
2020-05-28 21:32:21 +0800

    用代码实现了一个双向链表的结构

---
+++ 链表/bothWayNodeList.js
@@ -0,0 +1,27 @@
+
+function Node(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+}
+
+let node1 = new Node(1);
+let node2 = new Node(2);
+let node3 = new Node(3);
+let node4 = new Node(4);
+let node5 = new Node(5);
+
+node1.prev = null;
+node1.next = node2;
+node2.prev = node1;
+node2.next = node3;
+node3.prev = node2;
+node3.next = node4;
+node4.prev = node3;
+node4.next = node5;
+node5.prev = node4;
+node5.next = null;
+
+
+
+
