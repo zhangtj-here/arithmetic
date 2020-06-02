e906aede34d7a0ae79cc75b50190a1d4ab14bebe
zhangtj-here <15600788053@163.com>
2020-06-02 15:49:20 +0800

    将简单的非平衡二叉树，双旋变为平衡二叉树，有缺陷，当层级过深时，还原不了平衡二叉树

--- test.js
+++ test.js
@@ -5,13 +5,27 @@ function Node(value) {
 }
 
 let node2 = new Node('2');
-let node5 = new Node('5');
 let node3 = new Node('3');
+let node5 = new Node('5');
 let node6 = new Node('6');
+let node7 = new Node('7');
+let node8 = new Node('8');
+let node9 = new Node('9');
 
-node2.right = node5;
-node5.left = node3;
+// node9.left = node8;
+// node8.left = node7;
+// node7.left = node6;
+// node6.left = node5;
+// node5.left = node3;
+// node3.left = node2;
+
+
+node2.right = node3;
+node3.right = node5;
 node5.right = node6;
+node6.right = node7;
+node7.right = node8;
+node8.right = node9;
 
 
 function getDeep(root) {
@@ -58,13 +72,23 @@ function change(root) {
     let rightDeep = getDeep(root.right);
     if (Math.abs(leftDeep - rightDeep) < 2) return root;
     else {
-        if (leftDeep > rightDeep) return roteRight(root);
-        else return roteLeft(root);
+        if (leftDeep > rightDeep) {
+            let notChangeDeep = getDeep(root.left.left);
+            let changeDeep = getDeep(root.left.right);
+            if (changeDeep > notChangeDeep) root.left = roteLeft(root.left);
+            return roteRight(root);
+        }
+        else {
+            let notChangeDeep = getDeep(root.right.right);
+            let changeDeep = getDeep(root.right.left);
+            if (changeDeep > notChangeDeep) root.right = roteRight(root.right);
+            return roteLeft(root);
+        }
     }
 
 }
 
-console.log(change(node2));
+console.log(JSON.stringify(change(node2), null, "     "));
 
 
 // console.log(isBalance(node2));
